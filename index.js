2c53e2300d969d1948df3bce3b952d5a2960e39b
Mateusz Pyzowski <managervcf@gmail.com>
2018-11-28 21:42:38 +0800

    Add passport authentication logic using google strategy and OAuth.

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.get('*', (req, res) => 
-	res.send({ message: 'Hello World' })
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('accessToken', accessToken);
+			console.log('refreshToken', refreshToken);
+			console.log('profile', profile);
+		}
+	)
 );
 
-const port = process.env.PORT || 5000;
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 app.listen(port);
