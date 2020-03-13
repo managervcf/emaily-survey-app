18ae90220880b5ee369e0982ee05ed19ff99dcd1
Mateusz Pyzowski <managervcf@gmail.com>
2020-03-13 13:17:37 +0800

    Finish collecting user feddback and recording it in the database.

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,15 +1,83 @@
 const { model } = require('mongoose');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 
 const requireLogin = require('../middleware/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
 
-const Mailer = require('../services/mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const Mailer = require('../services/mailer');
 const Survey = model('Survey');
 
 module.exports = app => {
   // Route that end user is send back to after providing feedback
-  app.get('/api/surveys/thanks', (req, res) => res.send('Thanks for voting!'));
+  app.get('/api/surveys/:surveyId/:choice', (req, res) =>
+    res.send('Thanks for voting!')
+  );
+
+  app.post('/api/surveys/webhooks', async (req, res) => {
+    // Create a pathSchema providing a schema of our route
+    const pathSchema = new Path('/api/surveys/:surveyId/:choice');
+    // Print out entire sendgrid response body
+    console.log('\n\nRequestBody\n\n', req.body);
+
+    // Parse the post request from Sendgrid
+    const parsedEvents = req.body
+      // Map over and return an object with email, surveyId, choice or undefined
+      .map(({ url, email }) => {
+        // Extract path from the URL user clicked
+        const { pathname } = new URL(url);
+        // Test the path of the URL user clicked against our route schema
+        const urlMatch = pathSchema.test(pathname);
+        //  Return an object or undefined
+        return urlMatch
+          ? { email, surveyId: urlMatch.surveyId, choice: urlMatch.choice }
+          : undefined;
+      })
+      // Filter our undefined elements
+      .filter(event => event !== undefined)
+      // Remove duplicates where email and surveyId is the same
+      .filter(
+        (event, index, eventArray) =>
+          eventArray.findIndex(
+            e => e.email === event.email && e.surveyId === event.surveyId
+          ) === index
+      );
+
+    // Print out parsed events
+    console.log('\n\nParsed events\n\n', parsedEvents);
+
+    // Send empty object to sendgrid
+    res.send({});
+
+    //// Update database
+    // Find a survey with all the criteria and update it
+    const responses = await Promise.all(
+      parsedEvents.map(({ email, surveyId, choice }) =>
+        // Build a query
+        Survey.updateOne(
+          // Criteria
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email, responded: false }
+            }
+          },
+          // Update data
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        )
+          // Execute query
+          .exec()
+      )
+    );
+
+    // Print out response from MongoDB
+    console.log('\n\nResponse from MongoDB\n\n', responses);
+  });
 
   // Route handling creating new survey
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -29,7 +97,7 @@ module.exports = app => {
     let newSurvey = await Survey.create(survey);
 
     // Create new mailer instance
-    const newMailer = new Mailer(newSurvey, surveyTemplate(newSurvey.body));
+    const newMailer = new Mailer(newSurvey, surveyTemplate(newSurvey));
 
     try {
       // Send an email
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,17 +1,17 @@
 const { redirectDomain } = require('../../config/keys');
 
-module.exports = question => /*html*/ `
+module.exports = ({ body, id }) => /*html*/ `
   <html>
     <body>
       <div style="text-align: center">
         <h3>We would love your feedback</h3>
         <p>Please answer the following question:</p>
-        <p>${question}</p>
+        <p>${body}</p>
         <div>
-          <a href="${redirectDomain}/api/surveys/thanks">Yes</a>
+          <a href="${redirectDomain}/api/surveys/${id}/yes">Yes</a>
         </div>
         <div>
-          <a href="${redirectDomain}/api/surveys/thanks">No</a>
+          <a href="${redirectDomain}/api/surveys/${id}/no">No</a>
         </div>
       </div>
     </body>
