97db88e55e128a4fa59b65d79aac7c217576e5a2
Mateusz Pyzowski <managervcf@gmail.com>
2020-03-23 18:04:38 +0800

    Add a link to '/surveys' at the landing page.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div style={{ textAlign: 'center' }}>
     <h1>Welcome to Emaily!</h1>
     <p>Collect feedback from your users.</p>
+    <Link to="/surveys">Dashboard</Link>
   </div>
 );
 
